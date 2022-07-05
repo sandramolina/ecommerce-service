cb2b828de47d6353b283246992ddd834b8058380
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-07-05 21:59:58 +0100

    cleans up working version

--- src/main/java/com/reginageorge/ecommerceservice/repositories/PrNoLombokRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reginageorge.ecommerceservice.repositories;
-
-import com.reginageorge.ecommerceservice.models.PrNoLombok;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrNoLombokRepository extends JpaRepository<PrNoLombok, Long> {
-}
